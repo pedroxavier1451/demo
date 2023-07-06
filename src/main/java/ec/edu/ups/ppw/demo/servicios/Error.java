70828dff935c60d0ad31c1da7d96ee477a29b44e
pedroxavier1451 <pedro.xavier.orellana@gmail.com>
2023-07-06 07:58:14 -0500

    cambios

---
+++ src/main/java/ec/edu/ups/ppw/demo/servicios/Error.java
@@ -0,0 +1,22 @@
+package ec.edu.ups.ppw.demo.servicios;
+
+public class Error {
+	
+	private int codigo;
+	private String mensaje;
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getMensaje() {
+		return mensaje;
+	}
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+	
+	
+}
--- src/main/java/ec/edu/ups/ppw/demo/servicios/GClientesService.java
+++ src/main/java/ec/edu/ups/ppw/demo/servicios/GClientesService.java
@@ -5,19 +5,25 @@ import java.util.List;
 
 import ec.edu.ups.ppw.demo.datos.ClienteDAO;
 import ec.edu.ups.ppw.demo.modelo.Cliente;
+import ec.edu.ups.ppw.demo.negocio.GestionClientes;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.Response;
 
 @Path("clientes")
 public class GClientesService {
 	
+	@Inject
+	private GestionClientes gestionClientes;
 	
 	@Inject
 	private ClienteDAO clidao;
@@ -81,5 +87,21 @@ public class GClientesService {
 	    String result = sb.toString();
 	    System.out.print(result);
 	    return result;
+	}
+	
+	@POST
+	@Produces("application/json")
+	@Consumes("application/json")
+	public Response guardarCliente(Cliente cliente) {
+		try {
+			gestionClientes.guardarClientes(cliente);
+			return Response.status(Response.Status.OK).entity(cliente).build();
+		}catch(Exception e){
+			e.printStackTrace();
+			Error error = new Error();
+			error.setCodigo(99);
+			error.setMensaje("Error al guardar: " + e.getMessage());
+			return Response.status(Response.Status.OK).entity(error).build();
+		}
 	}
 }
