4bbbe1b2a1f8e55659b2a3146d9b6e327b0fd468
pedroxavier1451 <pedro.xavier.orellana@gmail.com>
2023-07-17 08:53:49 -0500

    c

---
+++ src/main/java/ec/edu/ups/ppw/demo/servicios/CORSFilter.java
@@ -0,0 +1,19 @@
+package ec.edu.ups.ppw.demo.servicios;
+
+import java.io.IOException;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class CORSFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        responseContext.getHeaders().add("Access-Control-Max-Age", "-1");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    }
+}
